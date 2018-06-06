3be298d88b24feb768a09a09ff60712b686e5ad3
unknown <patryk.senska@gmail.com>
2018-06-06 15:05:16 +0200

    Add GET /todos/:id query

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(todo){
+            res.send({todo});
+        }
+        else{
+            return res.status(404).send();
+        }
+    }, (err) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(3000, () => {
