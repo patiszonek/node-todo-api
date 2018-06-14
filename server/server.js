4b3607c6b31e849e459864342f8ef2e33a376c52
unknown <patryk.senska@gmail.com>
2018-06-14 09:35:16 +0200

    Added authentication method in separate file

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -113,6 +114,10 @@ app.post('/users', (req, res) => {
     .catch((err) => {
         res.status(400).send(err);
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
